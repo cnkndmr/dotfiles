6688ff4f9e70b7500dce700886163490d711451c
M. Can Kandemir <cnkndmr@gmail.com>
2020-04-17 17:13:20 +0300

    i3 dmenu_pdf script

--- i3/i3scripts/dmenu_pdf.sh
+++ i3/i3scripts/dmenu_pdf.sh
@@ -1,9 +1,18 @@
 #!/bin/sh
+case "$1" in
+	'-e')
+		editor=masterpdfeditor5
+		;;
+	*)
+		editor=evince
+		;;
+esac
+rm -rf ~/Documents/PDFs/uni-pdfs
 mkdir ~/Documents/PDFs/uni-pdfs
-rm ~/Documents/PDFs/uni-pdfs/*
-find ~/Dropbox/uni-pdfs -name "*\.pdf" -exec ln -s {} ~/Documents/PDFs/uni-pdfs \;
-choice=$(find ~/Documents/PDFs | grep ".pdf" | dmenu -p 'PDFS:' -i -l 20)
-#editor=$(echo "evince\nmasterpdfeditor5" | dmenu -p "Select PDF editor:")
-editor=evince
-[ "$(echo $choice | awk 'NF' | wc -l)" -eq "0" ] && exit
+lndir ~/Dropbox/uni-pdfs ~/Documents/PDFs/uni-pdfs
+choice=$(find ~/Documents/PDFs | grep "\.pdf" | dmenu -p 'PDFS:' -i -l 20)
+[ "$(echo "$choice" | awk 'NF' | wc -l)" -eq "0" ] && exit
+# editor=$(echo "evince\nmasterpdfeditor5" | dmenu -p "Select PDF editor:")
+# [ "$(echo "$editor" | awk 'NF' | wc -l)" -eq "0" ] && exit
+# editor=evince
 $editor "$choice"
