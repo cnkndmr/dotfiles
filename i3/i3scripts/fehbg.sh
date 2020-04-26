e48e978a0ca3f4b13a044d2162a7758b64bb92b6
M. Can Kandemir <cnkndmr@gmail.com>
2020-04-26 23:24:48 +0300

    i3scripts removed

--- i3/i3scripts/fehbg.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/sh
-feh --randomize --bg-fill "/home/$(whoami)/Documents/Mi8/Pictures/Walli Artworks/"
--- i3/i3scripts/move.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-for i in $(seq 2 9)
-do
-	i3-msg workspace "$i"
-	i3-msg move workspace to output left
-done
