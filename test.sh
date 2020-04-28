d35ac00074ab118461349b5ab02248c34c48d02d
M. Can Kandemir <cnkndmr@gmail.com>
2020-04-29 02:14:14 +0300

    test

--- test.sh
+++ test.sh
@@ -6,9 +6,9 @@ mkswap /dev/sda2
 swapon /dev/sda2
 mount /dev/sda1 /mnt
 curl -Ss "https://www.archlinux.org/mirrorlist/?country=DE&protocol=http&protocol=https&ip_version=4" | sed 's/^#//' > /etc/pacman.d/mirrorlist
-pacstrap /mnt base linux linux-firmware nano networkmanager
+pacstrap /mnt base linux linux-firmware nano networkmanager grub
 genfstab -U /mnt >> /mnt/etc/fstab
-arch-chroot /mnt /bin/bash << EOF
+arch-chroot /mnt << EOF
 ln -sf /usr/share/zoneinfo/Europe/Istanbul /etc/localtime
 hwclock –systohc
 echo "en_US.UTF-8 UTF-8" >> /etc/locale.gen
@@ -20,7 +20,7 @@ echo "127.0.0.1 localhost
 127.0.1.1 core.localdomain core" > /etc/hosts
 mkinitcpio -P
 echo "root:1423" | chpasswd
-pacman -S grub
+echo "====Password changed==="
 grub-install –target=i386-pc /dev/sda
 grub-mkconfig -o /boot/grub/grub.cfg
 EOF
