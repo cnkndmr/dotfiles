c0571a0a74306858e45d80bc788e960482132ce3
M. Can Kandemir <cnkndmr@gmail.com>
2020-07-23 02:23:09 +0300

    yay install merged

--- install.sh
+++ install.sh
@@ -1,9 +1,10 @@
-#!/bin/sh
+#!/bin/bash
 
 sudo pacman -Syu --noconfirm
 
 # Install packages
-./install_base.sh
+# ./install_base.sh
+sudo pacman -S --noconfirm --needed - < pkglist.txt
 
 # i3 setup
 mkdir -p ~/.config/i3
@@ -14,6 +15,12 @@ dconf load / < dconf
 mkdir -p ~/.emacs.d
 cp ./.emacs.d/init.el ~/.emacs.d
 
-./install_yay.sh
+## yay installation part
+sudo pacman -S --noconfirm git base-devel
+git clone https://aur.archlinux.org/yay.git
+cd yay
+makepkg -si
+cd ..
+rm -rf yay
 
 pkill -KILL -u "$(whoami)"
--- install_yay.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-sudo pacman -S --noconfirm git base-devel
-git clone https://aur.archlinux.org/yay.git
-cd yay
-makepkg -si
-cd ..
-rm -rf yay
