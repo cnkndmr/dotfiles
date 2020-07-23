f492950a422a081bd9c55c9307583e79275b65bd
M. Can Kandemir <cnkndmr@gmail.com>
2020-07-23 16:11:32 +0300

    yay install relocated

--- install.sh
+++ install.sh
@@ -2,8 +2,15 @@
 
 sudo pacman -Syu --noconfirm
 
-# Install packages
-# ./install_base.sh
+## yay installation part
+sudo pacman -S --noconfirm git base-devel
+git clone https://aur.archlinux.org/yay.git
+cd yay
+makepkg -si
+cd ..
+rm -rf yay
+
+# Install base packages
 sudo pacman -S --noconfirm --needed - < pkglist.txt
 
 # i3 setup
@@ -14,13 +21,5 @@ dconf load / < dconf
 
 mkdir -p ~/.emacs.d
 cp ./.emacs.d/init.el ~/.emacs.d
-
-## yay installation part
-sudo pacman -S --noconfirm git base-devel
-git clone https://aur.archlinux.org/yay.git
-cd yay
-makepkg -si
-cd ..
-rm -rf yay
 
 pkill -KILL -u "$(whoami)"
