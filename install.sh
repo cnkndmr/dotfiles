f1d12015ea8809d94f8f7300e9659c8635677bb5
M. Can Kandemir <cnkndmr@gmail.com>
2020-09-18 03:02:30 +0300

    zsh installation added

--- install.sh
+++ install.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 sudo pacman -Syu --noconfirm
-
+sudo pacman -S --noconfirm archlinux-keyring
 sudo pacman -S --noconfirm git
 
 git clone https://github.com/cnkndmr/dotfiles.git
@@ -12,7 +12,7 @@ cd dotfiles
 sudo pacman -S --noconfirm --needed - < pkglist.txt
 
 # yay installation part
-sudo pacman -S --noconfirm git base-devel
+sudo pacman -S --noconfirm base-devel
 git clone https://aur.archlinux.org/yay.git
 cd yay
 makepkg -si
@@ -23,10 +23,20 @@ rm -rf yay
 yay -S --noconfirm --needed - < pkglist_yay.txt
 
 # Copying configs
-cp -r ./home/.config/ ~/
-cp -r ./home/.emacs.d/ ~/
+cd ./home
+cp -R . ~/
+cd ..
+
+# Install zsh, antigen
+sudo pacman -S --noconfirm zsh
+mkdir -p ~/.config/antigen
+curl -L git.io/antigen > ~/.config/antigen/antigen.zsh
+chsh -s /usr/bin/zsh
 
 # Loading preferences
 dconf load / < dconf
+
+echo "Press enter to restart:"
+read
 
 pkill -KILL -u "$(whoami)"
